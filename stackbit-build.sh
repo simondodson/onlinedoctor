5d3d79f5ce39d01e08730e4bae6de3e759ba6479
SIMON DODSON <simondodson@gmail.com>
2020-06-30 01:45:12 +0700

    Update stackbit-build.sh

--- stackbit-build.sh
+++ stackbit-build.sh
@@ -5,7 +5,7 @@ set -o pipefail
 set -v
 
 curl -s -X POST https://api.stackbit.com/project/5ea31d8a4b50f6001ab043a5/webhook/build/pull > /dev/null
-if [[ -z "${34dcf861cb4e1c108a8229fed2dec01227e78d5598e0edb654623aad46472d54}" ]]; then
+if [[ -z "${STACKBIT_API_KEY}" ]]; then
     echo "WARNING: No STACKBIT_API_KEY environment variable set, skipping stackbit-pull"
 else
     npx @stackbit/stackbit-pull --stackbit-pull-api-url=https://api.stackbit.com/pull/5ea31d8a4b50f6001ab043a5 
